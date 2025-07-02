44135862c56c5ee3000539ae0146c78f05c51d59
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-02 17:03:34 +0200

    create readbyte method

--- nivell-2/src/Input.java
+++ nivell-2/src/Input.java
@@ -1,2 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Input {
+    private int number;
+    private static Scanner scanner = new Scanner(System.in);
+
+    // Mètodes a implantar capturant l’excepció de la classe "InputMismatchException":
+    //
+    //public static byte llegirByte(String missatge);
+    //
+    //public static int llegirInt(String missatge);
+    //
+    //public static float llegirFloat(String missatge);
+    //
+    //public static double llegirDouble(String missatge);
+
+    public static byte readByte(String message) {
+        while (true) {
+            System.out.println(message);
+            try {
+                return scanner.nextByte();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+    }
+
 }
