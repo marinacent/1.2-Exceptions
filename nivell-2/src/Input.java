d80b8baa5128e39fb65bba8a624415bc1d5f9131
marinacent <52744370+marinacent@users.noreply.github.com>
2025-07-02 17:23:48 +0200

    create readint, readfloat and readdouble methods

--- nivell-2/src/Input.java
+++ nivell-2/src/Input.java
@@ -2,18 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Input {
-    private int number;
     private static Scanner scanner = new Scanner(System.in);
-
-    // Mètodes a implantar capturant l’excepció de la classe "InputMismatchException":
-    //
-    //public static byte llegirByte(String missatge);
-    //
-    //public static int llegirInt(String missatge);
-    //
-    //public static float llegirFloat(String missatge);
-    //
-    //public static double llegirDouble(String missatge);
 
     public static byte readByte(String message) {
         while (true) {
@@ -21,10 +10,49 @@ public class Input {
             try {
                 return scanner.nextByte();
             } catch (InputMismatchException e) {
-                System.out.println(e.getMessage());
+                System.out.println("Format error");
+                scanner.next();
             }
         }
 
     }
 
+    public static int readInt(String message) {
+        while (true) {
+            System.out.println(message);
+            try {
+                return scanner.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Format error");
+                scanner.next();
+            }
+        }
+    }
+
+    public static float readFloat(String message) {
+        while (true) {
+            System.out.println(message);
+            try {
+                return scanner.nextFloat();
+            } catch (InputMismatchException e) {
+                System.out.println("Format error");
+                scanner.next();
+            }
+        }
+    }
+
+    public static double readDouble(String message) {
+        while (true) {
+            System.out.println(message);
+            try {
+                return scanner.nextDouble();
+            } catch (InputMismatchException e) {
+                System.out.println("Format error");
+                scanner.next();
+            }
+        }
+    }
+
 }
+
+
---
+++ nivell-2/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        byte myByte = Input.readByte("Please enter a byte: ");
+        System.out.println(myByte);
+
+        int myInt = Input.readInt("Please enter an int: ");
+        System.out.println(myInt);
+
+        float myFloat = Input.readFloat("Please enter a float: ");
+        System.out.println(myFloat);
+
+        double myDouble = Input.readDouble("Please enter a double: ");
+        System.out.println(myDouble);
+    }
+}
